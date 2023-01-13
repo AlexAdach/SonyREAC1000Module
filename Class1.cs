1a88d27ba3c10ba49e6298329b5ccb66cf1937d4
AlexAdach <32344954+AlexAdach@users.noreply.github.com>
2023-01-13 11:19:37 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonyREAC1000Module
+{
+    public class Class1
+    {
+    }
+}
